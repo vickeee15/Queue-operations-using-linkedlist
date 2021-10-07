c7a154af21eced4e5ef507d7537455ff4eae061c
vickeee15 <89180138+vickeee15@users.noreply.github.com>
2021-10-07 16:47:02 +0530

    Add files via upload

--- MainClass.java
+++ MainClass.java
@@ -1,12 +1,11 @@
-import java.util.Queue;
-
 public class MainClass {
     public static void main(String[] args) {
 
-        Enqueue<Integer> queue = new Enqueue<>();
+        Queue<Integer> queue = new Queue<>();
         queue.enQueue(56);
         queue.enQueue(30);
         queue.enQueue(70);
+        queue.deQueue();
         queue.displayQueue();
     }
 }
